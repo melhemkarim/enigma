935502aca47d8a0f5c00c0179f09d314f14e673a
karim <melhemkarim@outlook.com>
2024-04-21 13:52:27 +0300

    //

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='  bg-gray-800 h-screen flex flex-col justify-center '>
       <h1 className=' text-6xl font-serif font-black text-white m-auto pt-52 '>WE'RE TAKING OVER SOON</h1>\
-      <img className=' h-auto w-auto m-auto pb-32' src="logo.png" alt="" />
+      <img className=' h-auto w-auto m-auto pb-32' src="logo.png" alt="" />\
     </div>
     
     
