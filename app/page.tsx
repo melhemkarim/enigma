b1387a429c6d116b91661aa427b87a1666565ed2
karim <melhemkarim@outlook.com>
2024-04-21 14:37:24 +0300

    //

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='  bg-gray-800 h-screen flex flex-col justify-center '>
-      <h1 className=' text-6xl font-serif font-black text-white m-auto pt-52 '>WE'RE TAKING OVER SOON</h1>\
-      <img className=' h-auto w-auto m-auto pb-32' src="logo.png" alt="" />\
+    <div className='  bg-gray-950 h-screen flex flex-col justify-center  m-auto'>
+      <img className=' h-auto w-auto m-auto  ' src="logo-white.png" alt="" />
+      <h1 className=' text-6xl font-serif font-black text-white m-auto  '>WE'RE TAKING OVER SOON</h1>\
+      <img className=' h-auto w-auto m-auto pb-32' src="logo.png" alt="" />
     </div>
     
     
